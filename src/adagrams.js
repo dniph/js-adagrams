2387955702acf05282b2355f62136d42f2b4dd0f
Dennif <dennif.pa@gmail.com>
2025-05-22 14:52:53 -0700

    wave 01 draw letters implemented

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,32 @@
+
+const LETTER_DISTRIBUTION = {
+  A: 9, B: 2, C: 2, D: 4, E: 12,
+  F: 2, G: 3, H: 2, I: 9, J: 1,
+  K: 1, L: 4, M: 2, N: 6, O: 8,
+  P: 2, Q: 1, R: 6, S: 4, T: 6,
+  U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
+};
+
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letterPool = [];
+  for (const [letter, count] of Object.entries(LETTER_DISTRIBUTION)) {
+    for (let i = 0; i <count; i++){
+      letterPool.push(letter);
+    }
+  }
+
+  const hand = [];
+
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * letterPool.length);
+    const chosenLetter = letterPool[randomIndex];
+    hand.push(chosenLetter);
+    letterPool.splice(randomIndex, 1); // eliminamos la letra usada
+  }
+
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
