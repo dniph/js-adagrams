998d4d4ce7c29cd5f7128aacd014a2bb045d92d8
Dennif <dennif.pa@gmail.com>
2025-05-27 15:10:54 -0700

    wave 02 finished

--- src/adagrams.js
+++ src/adagrams.js
@@ -30,7 +30,19 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  const handCopy = [...lettersInHand];
+
+
+  for (let char of input.toUpperCase()) {
+    const index = handCopy.indexOf(char);
+    if (index === -1) {
+      return false; 
+    }
+    handCopy.splice(index, 1); 
+  }
+
+  return true; 
+
 };
 
 export const scoreWord = (word) => {
